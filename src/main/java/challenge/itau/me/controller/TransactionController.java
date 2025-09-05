6905d47105b7b384fcd1527c3f50b4a93e2829e4
Manoel <vinesouza9x8@gmail.com>
2025-09-05 12:35:49 -0300

    criação de endpoint Delete: /transacao. Limpa por completo a fila de transacoes

--- src/main/java/challenge/itau/me/controller/TransactionController.java
+++ src/main/java/challenge/itau/me/controller/TransactionController.java
@@ -6,10 +6,7 @@ import challenge.itau.me.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
@@ -25,6 +22,12 @@ public class TransactionController {
     TransactionRequestDto requestDto){
         transactionService.addTransaction(new Transaction(requestDto.valor(), requestDto.dataHora()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> clearTransactions() {
+        transactionService.clearTransactions();
+        return ResponseEntity.ok().build();
     }
 
 
