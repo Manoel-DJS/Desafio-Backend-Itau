615b5f0355a897c71e31451de9468f1acff00ae7
Manoel <vinesouza9x8@gmail.com>
2025-09-05 11:14:37 -0300

    inicial commit

---
+++ src/main/java/challenge/itau/me/MeApplication.java
@@ -0,0 +1,13 @@
+package challenge.itau.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeApplication.class, args);
+	}
+
+}
