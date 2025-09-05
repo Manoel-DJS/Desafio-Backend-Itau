397233c5794bde295eee7a18939b3730150cae0f
Manoel <vinesouza9x8@gmail.com>
2025-09-05 11:22:07 -0300

    criação da entidade Transaction

---
+++ src/main/java/challenge/itau/me/model/Transaction.java
@@ -0,0 +1,29 @@
+package challenge.itau.me.model;
+
+import java.time.OffsetDateTime;
+
+public class Transaction {
+    private double valor;
+    private OffsetDateTime dataHora;
+
+    public Transaction(double valor, OffsetDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(OffsetDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+}
